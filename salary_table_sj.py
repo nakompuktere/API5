a8d3a5177cd9d6181ee5e398f26c62a29d25d588
nakompuktere <144929164+nakompuktere@users.noreply.github.com>
2024-06-07 17:39:30 +0300

    Посчитал все страницы

--- salary_table_sj.py
+++ salary_table_sj.py
@@ -1,5 +1,6 @@
 import requests
 from tools import predict_rub_salary
+from itertools import count
 
 
 def get_sj_statistic(sj_secret_key):
@@ -12,7 +13,7 @@ def get_sj_statistic(sj_secret_key):
     for language in languages:
         vacancies_processed_sj = 0
         average_vacancy_salary = 0
-        for page in range(5):
+        for page in count(0, 1):
 
             headers = {
                 "X-Api-App-Id": sj_secret_key
@@ -35,11 +36,13 @@ def get_sj_statistic(sj_secret_key):
                     average_vacancy_salary += predict_rub_salary(vacancy["payment_from"], vacancy["payment_to"])
                     if average_vacancy_salary > 0:
                         vacancies_processed_sj += 1
+            if not response.json()["more"]:
+                break
 
-            if vacancies_processed_sj:
-                average_salary_sj = int(average_vacancy_salary/vacancies_processed_sj)
-            else:
-                average_salary_sj = 0
+        if vacancies_processed_sj:
+            average_salary_sj = int(average_vacancy_salary/vacancies_processed_sj)
+        else:
+            average_salary_sj = 0
                 
         vacancies_sj[language] = {
             "average_salary": average_salary_sj,
