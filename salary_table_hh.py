adf2f37044e1ebf20b838b019da3c8998c39b7e3
nakompuktere <144929164+nakompuktere@users.noreply.github.com>
2024-06-12 17:12:49 +0300

    Сделал код более плоским

--- salary_table_hh.py
+++ salary_table_hh.py
@@ -30,11 +30,10 @@ def get_hh_statistic():
 
             for vacancy in vacancies:
                 vacancy_salary = vacancy["salary"]
-                if vacancy_salary:
-                    if vacancy_salary["currency"] == "RUR":
-                        total_salary += predict_rub_salary(vacancy_salary["from"], vacancy_salary["to"])
-                        if total_salary > 0:
-                            vacancies_processed += 1
+                if vacancy_salary and vacancy_salary["currency"] == "RUR":
+                    total_salary += predict_rub_salary(vacancy_salary["from"], vacancy_salary["to"])
+                    if total_salary > 0:
+                        vacancies_processed += 1
         try:
             average_salary = int(total_salary/vacancies_processed)
         except ZeroDivisionError:
